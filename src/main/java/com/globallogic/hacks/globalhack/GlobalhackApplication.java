0bade006d2ff006082935a0a79182502d49235e6
Darshan <dn@marketo.com>
2019-04-24 14:59:50 +0530

    first commit

---
+++ src/main/java/com/globallogic/hacks/globalhack/GlobalhackApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.hacks.globalhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalhackApplication.class, args);
+	}
+
+}
